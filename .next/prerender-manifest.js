1a65c2273e81ab3c126ca67b9aac151bafc0b069
Uday Jawheri <jawheriuday@gmail.com>
2024-02-20 21:31:31 +0530

    .next add

---
+++ .next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"preview\":{\"previewModeId\":\"021b8365e6b775db5bde6c7ae9bcb49f\",\"previewModeSigningKey\":\"518fdb226c1f478bdbab4f92243889f726756379fc4c763f6f8312efec2d960d\",\"previewModeEncryptionKey\":\"f8aaa267e54759f44ae4e369836eb6b25ab20c9012d6e1b9b8a77bc916b72b2e\"}}"
